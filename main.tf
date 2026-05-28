78a95cddd0ea2736489ae5b96215da80d7b8d540
Roland Bapst <roland.bapst@tamedia.ch>
2026-05-28 13:09:07 +0200

    feat(backup_monitor): allow crowler to get AWS account alias name

--- main.tf
+++ main.tf
@@ -62,6 +62,14 @@ data "aws_iam_policy_document" "backup_monitor_crawler_policy" {
     ]
     resources = ["*"]
   }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:ListAccountAliases",
+    ]
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_policy" "backup_monitor_crawler_policy" {
